2eb209c874f72b3a341cb6348ed4ce2a908eaa4c
smarbos <smarbos@gmail.com>
2016-01-06 17:20:36 -0300

    Ahora las rutas estan funcionando. Añadido ui.router

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -1,63 +1,21 @@
 'use strict';
 
-/* App Module */
+var adopteitorApp = angular.module('adopteitorApp', ['ui.router']);
 
-var adopteitorApp = angular.module('adopteitorApp', [
-	'config',
-	'ngCookies',
-	'ngRoute',
-	'angular-ui',
-	'ui.bootstrap'
-])  .config(function ($routeProvider) {
-    $routeProvider.
-          when('/login', {
-            templateUrl: 'views/login.html',
-            controller: 'LoginCtrl',
-            resolve: {
-              authenticated: ['djangoAuth', function(djangoAuth){
-                return djangoAuth.authenticationStatus();
-              }],
-            }
-          }).
-          when('/logout', {
-            templateUrl: 'views/logout.html',
-            controller: 'LogoutCtrl',
-            resolve: {
-              authenticated: ['djangoAuth', function(djangoAuth){
-                return djangoAuth.authenticationStatus();
-              }],
-            }
-          }).
-          when('/admin', {
-            templateUrl: 'views/admin.html',
-            controller: 'AdminCtrl',
-          }).
-		  when('/home', {
-            templateUrl: 'views/home.html',
-            controller: 'HomeCtrl',
-          }).
-          when('/libraries/:libraryId', {
-            templateUrl: 'views/library-detail.html',
-            controller: 'LibraryDetailCtrl',
-            resolve: {
-              authenticated: ['djangoAuth', function(djangoAuth){
-                return djangoAuth.authenticationStatus(true);
-              }],
-            }
-          }).
-          when('/viewers/:libraryId', {
-            templateUrl: 'views/viewers.html',
-            controller: 'ViewerCtrl',
-            resolve: {
-              authenticated: ['djangoAuth', function(djangoAuth){
-                return djangoAuth.authenticationStatus();
-              }],
-            }
-          }).
-          otherwise({
-            redirectTo: '/'
-          });
-  })
-  .run(function(djangoAuth, ENV){
-    djangoAuth.initialize(ENV['apiEndpoint']+'rest-auth', false);
-  });
+
+adopteitorApp.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+
+        .state('home', {
+            url: '/home',
+            templateUrl: 'views/home.html'
+        })
+
+        .state('about', {
+            // we'll get to this in a bit
+        });
+
+});
