dc8295ad8be1c35f8b777ad069d8c1831df51817
smarbos <smarbos@gmail.com>
2016-01-06 17:52:39 -0300

    Añadidas secciones donaciones, mision, y los menues

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -13,9 +13,29 @@ adopteitorApp.config(function($stateProvider, $urlRouterProvider) {
             url: '/home',
             templateUrl: 'views/home.html'
         })
-
-        .state('about', {
-            // we'll get to this in a bit
+        .state('mision', {
+            url: '/mision',
+            templateUrl: 'views/mision.html'
+        })
+        .state('rescatados', {
+            url: '/rescatados',
+            templateUrl: 'views/rescatados.html'
+        })
+        .state('historias', {
+            url: '/historias',
+            templateUrl: 'views/historias.html'
+        })
+        .state('shop', {
+            url: '/shop',
+            templateUrl: 'views/shop.html'
+        })
+        .state('contacto', {
+            url: '/contacto',
+            templateUrl: 'views/contacto.html'
+        })
+        .state('donaciones', {
+            url: '/donaciones',
+            templateUrl: 'views/donaciones.html'
         });
 
 });
